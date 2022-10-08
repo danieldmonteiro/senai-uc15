7d0edcc2f68475e445cafcaf9e1dc71dd59d077c
danieldmonteiro <danieldmonteiro@gmail.com>
2022-10-08 18:21:52 -0300

    Encontro Remoto 1

---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Calculadora
+{
+	public static class Calculadora
+	{
+		
+			public static double Somar(double pNum, double sNum)
+            {
+				return (pNum + sNum);
+            }
+		
+	}
+
+	//Para classes comuns:
+    //Calculadora calc = new Calcularora;
+	//Calc.Somar();
+
+	//Para classes estáticas (static):
+	//Calculadora.Somar();
+}
+
+
+
---
+++ Calculadora/TesteXUnit/TesteCalcXUnit.cs
@@ -0,0 +1,45 @@
+﻿using Calculadora;
+
+namespace TesteXUnit
+
+{ 
+
+    public class TesteCalcXUnit
+    {
+        [Fact]
+        public void SomarDoisNumeros()
+        {
+            //Arrange:
+            double pNum = 1;
+            double sNum = 2;
+            double rNum = 3;
+
+            //Act:
+
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert:
+
+            Assert.Equal(rNum, resultado);
+
+        }
+
+        [Theory]
+        [InLineData(1, 1, 2)]
+        [InLineData(2, 2, 4)]
+        [InLineData(2, 3, 5)]
+
+        public void SomarDoisNumerosLista(double pNum, double sNum, double rNum)
+        {
+            //Act:
+
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert:
+
+            Assert.Equal(rNum, resultado);
+
+        }
+
+    }
+}
