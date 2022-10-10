c6deeb918961da9a4788fbfdb7c290ade63385b4
danieldmonteiro <danieldmonteiro@gmail.com>
2022-10-10 19:46:45 -0300

    Encontro remoto 2

---
+++ CalcImc/TesteXUnit/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using CalcImc;
+
+namespace TesteXUnit;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(100, 1.7, "Obesidade grau 1")]
+    [InlineData(90, 1.8, "Sobrepeso")]
+    [InlineData(60, 1.6, "Peso normal")]
+    public void TesteCalcImc(double kg, double altura, string resultadoTeste)
+    {
+        //Act
+        var resultadoImc = Calculadora.CalcularImc(kg, altura);
+
+        //Assert
+        Assert.Equal(resultadoTeste, resultadoImc);
+    }
+}
